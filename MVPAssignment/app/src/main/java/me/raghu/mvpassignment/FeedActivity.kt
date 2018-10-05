d4d11a7c66ab0b8b98db84730de58016619f8532
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2018-10-05 10:39:18 +0530

    Initial Commit

---
+++ MVPAssignment/app/src/main/java/me/raghu/mvpassignment/FeedActivity.kt
@@ -0,0 +1,12 @@
+package me.raghu.mvpassignment
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FeedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
