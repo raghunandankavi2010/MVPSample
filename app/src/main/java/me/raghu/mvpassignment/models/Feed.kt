5dee265c20646eca38d86fa286c069df9b5d0369
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2018-12-20 16:04:10 +0530

    Remove unwanted import statements.
    Remove unwanted import statements.

--- app/src/main/java/me/raghu/mvpassignment/models/Feed.kt
+++ app/src/main/java/me/raghu/mvpassignment/models/Feed.kt
@@ -1,7 +1,5 @@
 package me.raghu.mvpassignment.models
 
-import com.google.gson.annotations.Expose
-import com.google.gson.annotations.SerializedName
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
